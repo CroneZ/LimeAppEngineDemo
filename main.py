4811478b293ce166d27c8d314297940c9743f089
CroneZ <sowcw1@hotmail.com>
2021-08-03 10:22:49 +0000

    refactored for app engine standard

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[27]:
+# In[22]:
 
 
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, send_from_directory
 import lime
 from lime import lime_image
 from skimage.segmentation import mark_boundaries
@@ -12,16 +12,21 @@ import matplotlib.pyplot as plt
 from skimage.transform import resize
 import tensorflow as tf
 import os
-import cv2
+import skimage.io
 
 
-# In[28]:
+# In[23]:
 
+
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+model = tf.keras.models.load_model('model/')
 
 
-# In[29]:
+# In[24]:
 
 
 @app.route("/")
@@ -29,26 +34,49 @@ def home():
     return render_template("index.html")
 
 
-# In[30]:
+# In[25]:
+
+
+@app.route('/tmp/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+
+# In[26]:
 
 
 @app.route("/explanation", methods = ['POST'])
 def explanation():
     image = request.files['filename']
-    image.save(os.path.join(os.getcwd(),"static","image.jpg"))
-    image = cv2.imread('static/image.jpg')
+    image.save(os.path.join("/tmp/image.jpg"))
+    image = skimage.io.imread(fname='/tmp/image.jpg')
     explainer = lime_image.LimeImageExplainer()
-    model = tf.keras.models.load_model('model/')
     image = resize(image, (150, 150))
     
     explanation = explainer.explain_instance(image, model.predict, top_labels=2, hide_color=0, num_samples=1000)
     
+    path1 = "/tmp/figure1.png"
+    path2 = "/tmp/figure2.png"
+
+    #remove any old figure1 if exist
+    try:
+        os.remove(path1)
+    except:
+        print("File for path1 does not exist")
     temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=True)
-    plt.imsave(os.path.join(os.getcwd(),"static","figure1.png"),mark_boundaries(temp , mask))
+    plt.imsave(path1,mark_boundaries(temp , mask))
     
+    #remove any old figure2 if exist
+    try:
+        os.remove(path2)
+    except:
+        print("File for path2 does not exist")
     temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=False)
-    plt.imsave(os.path.join(os.getcwd(),"static","figure2.png"),mark_boundaries(temp , mask))
+    plt.imsave(path2,mark_boundaries(temp , mask))
     
+    while True:
+        if(os.path.isfile(path1) and os.path.isfile(path2)):
+            break
     return render_template("explanation.html")
 
 
@@ -56,7 +84,7 @@ def explanation():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=8080)
 
 
 # In[ ]:
